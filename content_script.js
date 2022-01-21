d12d98db4ed9e853030989b807dba54a2df09352
Varun Chilukuri <varunch321@gmail.com>
2022-01-21 15:08:34 -0500

    Trim and use best practices

--- content_script.js
+++ content_script.js
@@ -1,26 +1,25 @@
-var links = document.getElementsByTagName("a");
-const key = "total_trackers_blocked"
-const key_block = "sites_to_turn_off_blocking"
+const key = "total_trackers_blocked";
+const key_block = "sites_to_turn_off_blocking";
 
-chrome.storage.local.get(['key_block'], function (result) {
+let links = document.getElementsByTagName("a");
+chrome.storage.local.get(["key_block"], function (result) {
     let contains = false;
-    for (var i = 0; i < result.key_block.length; i++) {
-        if (JSON.stringify(result.key_block[i]) === JSON.stringify(new URL(location.href).origin)) {
+    for (let i = 0; i < result.key_block.length; i++) {
+        if (result.key_block[i] === new URL(location.href).origin) {
             contains = true;
         }
     }
     if (!contains) {
-        for (var i = 0; i < links.length; i++) {
+        for (let i = 0; i < links.length; i++) {
             let long_link = links[i].href;
-            let short_link = long_link.replace(/utm_[^&]+&?/g, '');
-            short_link = short_link.replace(/fbclid[^&]+&?/g, '');
+            let short_link = long_link.replace(/utm_[^&]+&?/g, "");
+            short_link = short_link.replace(/fbclid[^&]+&?/g, "");
             if (short_link != long_link) {
-                chrome.storage.local.get(['key'], function (result) {
-                    let old_num = result.key
-                    let new_num = parseInt(old_num) + 1
-                    new_num = new_num.toString()
+                chrome.storage.local.get(["key"], function (result) {
+                    let new_num = parseInt(result.key) + 1;
+                    new_num = new_num.toString();
                     chrome.storage.local.set({ key: new_num }, function () {
-                        console.log(new_num)
+                        console.log(new_num);
                     });
                 });
             }
@@ -29,8 +28,7 @@ chrome.storage.local.get(['key_block'], function (result) {
     }
 });
 
-var title = document.getElementById("title");
-chrome.storage.local.get(['key'], function (result) {
-    let new_num = result.key
-    title.innerHTML = new_num
+let title = document.getElementById("title");
+chrome.storage.local.get(["key"], function (result) {
+    title.innerHTML = result.key;
 });
