1beba727e0c67d7069612727263475c4e3f79aee
Varun Chilukuri <varunch321@gmail.com>
2022-01-21 15:06:54 -0500

    Reformat code with Prettier

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-const key = "total_trackers_blocked"
-const val = "0"
+const key = "total_trackers_blocked";
+let val = "0";
 
-chrome.storage.local.set({key: val}, function(){
-    console.log("success")
+const key_block = "sites_to_turn_off_blocking";
+let val_block = [];
+
+chrome.storage.local.set({ key: val }, function () {
+  console.log("success");
 });
 
-
-const key_block = "sites_to_turn_off_blocking"
-const val_block = []
-
-chrome.storage.local.set({key_block: val_block}, function(){
-    console.log("success_block")
+chrome.storage.local.set({ key_block: val_block }, function () {
+  console.log("success_block");
 });
