85b0274c657eafa8d586cd4cfa66b3a796a5f95e
Varun Chilukuri <varunch321@gmail.com>
2022-01-22 22:34:43 -0500

    Reformat content_script.js

--- content_script.js
+++ content_script.js
@@ -3,32 +3,32 @@ const key_block = "sites_to_turn_off_blocking";
 
 let links = document.getElementsByTagName("a");
 chrome.storage.local.get(["key_block"], function (result) {
-    let contains = false;
-    for (let i = 0; i < result.key_block.length; i++) {
-        if (result.key_block[i] === new URL(location.href).origin) {
-            contains = true;
-        }
+  let contains = false;
+  for (let i = 0; i < result.key_block.length; i++) {
+    if (result.key_block[i] === new URL(location.href).origin) {
+      contains = true;
     }
-    if (!contains) {
-        for (let i = 0; i < links.length; i++) {
-            let long_link = links[i].href;
-            let short_link = long_link.replace(/utm_[^&]+&?/g, "");
-            short_link = short_link.replace(/fbclid[^&]+&?/g, "");
-            if (short_link != long_link) {
-                chrome.storage.local.get(["key"], function (result) {
-                    let new_num = parseInt(result.key) + 1;
-                    new_num = new_num.toString();
-                    chrome.storage.local.set({ key: new_num }, function () {
-                        console.log(new_num);
-                    });
-                });
-            }
-            links[i].href = short_link;
-        }
+  }
+  if (!contains) {
+    for (let i = 0; i < links.length; i++) {
+      let long_link = links[i].href;
+      let short_link = long_link.replace(/utm_[^&]+&?/g, "");
+      short_link = short_link.replace(/fbclid[^&]+&?/g, "");
+      if (short_link != long_link) {
+        chrome.storage.local.get(["key"], function (result) {
+          let new_num = parseInt(result.key) + 1;
+          new_num = new_num.toString();
+          chrome.storage.local.set({ key: new_num }, function () {
+            console.log(new_num);
+          });
+        });
+      }
+      links[i].href = short_link;
     }
+  }
 });
 
 let title = document.getElementById("title");
 chrome.storage.local.get(["key"], function (result) {
-    title.innerHTML = result.key;
+  title.innerHTML = result.key;
 });
