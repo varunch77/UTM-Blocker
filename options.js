5d6798b6d8ddbae92236c3f542516330b25ef7f9
Varun Chilukuri <varunch321@gmail.com>
2022-01-30 11:23:59 -0500

    Fix theme setting to persist on options page

--- options.js
+++ options.js
@@ -1,10 +1,21 @@
+window.onload = function () {
+    chrome.storage.local.get(["key_theme"], function (theme) {
+        if (theme.key_theme === "light") {
+        }
+        else {
+            var checkbox = document.getElementById("theme");
+            checkbox.setAttribute("checked", "");
+        }
+    });
+};
+
 document.getElementById("theme").addEventListener("change", function () {
     if (this.checked) {
         chrome.storage.local.set({ key_theme: "dark" }, function () {
             console.log("theme preference updated");
         });
     }
-    else{
+    else {
         chrome.storage.local.set({ key_theme: "light" }, function () {
             console.log("theme preference updated");
         });
